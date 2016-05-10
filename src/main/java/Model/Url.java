77f254f2584987688c5558fb3afb82ab43188739
Vik <vnazarevich@gmail.com>
2016-05-10 16:17:21 +0300

    first commit

---
+++ src/main/java/Model/Url.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class Url {
+	private String originalUrl;
+	private String tinyUrl;
+	
+	public String getOriginalUrl() {
+		return originalUrl;
+	}
+	public void setOriginalUrl(String originalUrl) {
+		this.originalUrl = originalUrl;
+	}
+	public String getTinyUrl() {
+		return tinyUrl;
+	}
+	public void setTinyUrl(String tinyUrl) {
+		this.tinyUrl = tinyUrl;
+	}
+	
+	
+
+}
